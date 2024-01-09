74abac4c0c95038084a138b84dea97c02f277651
Noel Johansson <noel.johansson@elev.ga.ntig.se>
2024-01-09 11:18:45 +0100

    indexrouter

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 const express = require("express")
 const router = express.Router()
-const indexRouter = require("./routes/index")
 
-app.use("/", indexRouter)
 router.get("/", function (req, res) {
-  res.render('index.njk', { title: 'Hello from template' })
+  res.render('index.njk', { title: 'Greatest burritos in this galaxy!' })
+})
+
+router.get("/meny", function (req, res) {
+  res.render('meny.njk', { title: 'Welcome to the greatest burrito menu!!!' })
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,29 +1,31 @@
 const express = require("express")
 const res = require("express/lib/response")
 const app = express();
-const indexRouter = require ('./routes/index')
+const indexRouter = require('./routes/index')
 const port = process.env.PORT || 3000
 const nunjucks = require('nunjucks');
- 
+
 nunjucks.configure
-('views', {
-    autoescape: true,
-    express: app
-});
+    ('views', {
+        autoescape: true,
+        express: app
+    });
+
+app.use(express.static('public'))
+app.use((req, res, next) => {
+    res.locals.url = req.originalUrl
+    next()
+})
 
 app.use('/', indexRouter)
-app.use(express.static('public'))
-
-app.get("/TRE", (req, res) => {
-    res.send("Hejsan svejsan")
-})
-app.get("/", (req, res) => {
-    res.render('index.njk', {title: 'Hello from template'})
-})
 
 
 
-app.listen(port,()=> {
+
+
+
+
+app.listen(port, () => {
     console.log(`server running on port
     ${port}`)
 })
